1cf7bfc4f57f9cf1cef85e525a0f5b8ee861e71b
manojpudasaini <mpudasaini17@gmail.com>
2021-05-03 17:47:18 +0545

    wip: about section

--- components/organism/eventdetail/index.tsx
+++ components/organism/eventdetail/index.tsx
@@ -5,18 +5,18 @@ import {Button} from "semantic-ui-react"
 const DetailWrapper= Styled.div`
     max-width: 600px;
     width: 100%;
-   // border: 2px solid black;
-    padding: 20px;
+    border: 2px solid black;
 `;
 
 const HeadWrapper= Styled.div`
-    padding: 0px 20px;
+    padding: 20px;
     h1{
-        font-size: 2.6rem;
+        font-size: 2.5rem;
         text-transform: capitalize;
         font-weight: 800;
         font-family: Jost;
         letter-spacing: 1.2px;
+        line-height: 1.2;
     }
     p{
         font-family: Jost;
@@ -33,7 +33,8 @@ const HeadWrapper= Styled.div`
 `;
 
 const BodyWrapper= Styled.div`
-   padding: 20px;
+   padding: 10px 20px;
+   border-bottom: 1px solid lightgray;
 `;
 
 const BodyItem= Styled.div`
@@ -42,12 +43,11 @@ const BodyItem= Styled.div`
     align-items: baseline;
     font-size: 18px;
     border-bottom: 1px solid lightgray;
-    margin-top: 25px;
+    margin-top: 20px;
      p{
         text-transform: uppercase ;
         font-weight: 600;
-        font-family: Jost;
-        
+        font-family: Jost;  
     }
     span{
         font-weight: 500;
@@ -55,13 +55,24 @@ const BodyItem= Styled.div`
 `;
 
 const ButtonContainer= Styled.div`
-    padding: 25px 0px;
+    padding: 25px 0px 15px 0px;
     display: flex;
     @media(max-width: 800px){
         justify-content: center;
     }
 `;
 
+const AboutWrapper= Styled.div`
+    p{
+        text-align: justify;
+        text-justify: inter-word;
+        text-align: left;
+        font-size: 18px;
+        padding: 0px 20px;
+        line-height: 1.6;
+    }
+     border-bottom: 1px solid lightgray;
+`;
 const EventDetail = () => {
     return (
         <DetailWrapper>
@@ -86,8 +97,14 @@ const EventDetail = () => {
                 <ButtonContainer>
                     <Button inverted color="orange" size="big">Join Event</Button>
                 </ButtonContainer>
-                
-            </BodyWrapper>     
+            </BodyWrapper>  
+
+            <AboutWrapper>
+                <HeadWrapper>
+                    <h1>ABOUT EVENT</h1>
+                </HeadWrapper>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, maxime suscipit ex nesciunt harum cumque rem assumenda. Nobis quo temporibus esse dolores voluptatem animi eligendi accusamus, odit voluptatum dicta corporis exercitationem. Fugiat numquam inventore, tenetur sunt ipsam molestias excepturi tempora? Error, natus sit. Ipsam explicabo officiis fugit quos esse. Facere, esse mollitia dolorum placeat consequatur doloremque hic enim tenetur vel nemo excepturi fugit id similique quaerat error repudiandae ipsum quam, nesciunt, quo magni corporis nam sunt? Totam quam sit expedita consequuntur debitis consequatur aliquid, corrupti odit nisi reiciendis delectus quia aliquam dignissimos nobis necessitatibus. Eius voluptas laudantium maiores porro iusto molestiae fugit. Delectus deleniti ipsum nihil exercitationem? Minima, dignissimos voluptate repellendus mollitia, ducimus possimus veritatis cumque ab dolores nihil autem temporibus repudiandae necessitatibus dolor voluptatem? Iure numquam voluptates ex explicabo dolores nam! Facilis et doloribus est dolorum, recusandae sint incidunt! Totam exercitationem quia assumenda recusandae quae incidunt quisquam, repellendus impedit?</p>
+            </AboutWrapper>
         </DetailWrapper>
     )
 }
